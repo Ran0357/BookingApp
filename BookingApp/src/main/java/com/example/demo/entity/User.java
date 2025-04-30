02493a944d1535ac89ad08599265d6bad914a553
takeda <takaki1257@gmail.com>
2025-04-30 11:05:06 +0900

    新規登録とユーザープロフィール作りました

--- BookingApp/src/main/java/com/example/demo/entity/User.java
+++ BookingApp/src/main/java/com/example/demo/entity/User.java
@@ -1,11 +1,71 @@
-//package com.example.demo.entity;
-//
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.Table;
-//
-//@Entity
-//@Table(name = "user")
-//public class User {
-//	
-//	
-//}
+package com.example.demo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 自動採番を設定
+    @Column(name = "id", length = 16)
+    private int id;
+    
+    @Column(name = "username", length = 64)
+    private String username;
+    
+    @Column(name = "userid", length = 8)
+    private String userid;
+    
+    @Column(name = "password", length = 16)
+    private String password;
+    
+    @Column(name = "usertype", length = 1)
+    private int usertype;
+
+    // Getter & Setter
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getUsertype() {
+        return usertype;
+    }
+
+    public void setUsertype(int usertype) {
+        this.usertype = usertype;
+    }
+}
